9135fec01a165c2034a690ffc81322d79ee48001
Piotr Baran <piotr.alki@gmail.com>
2017-03-29 14:00:55 +0200

    update

--- src/app/components/HomePage/CategoryBanner.jsx
+++ src/app/components/HomePage/CategoryBanner.jsx
@@ -8,8 +8,15 @@ var CategoryBanner = React.createClass({
     return (
       <div className="CategoryBanner-Row">
         <a href="#" className="CategoryBanner-Banner">
-          <img src="img/sports.jpg" alt="" />
-          <span className="Button ButtonSecondary">Button</span>
+          <span className="CategoryBanner-BannerHoverContent">
+            <span className="CategoryBanner-BannerHoverBox">
+              <span className="CategoryBanner-BannerHoverName">Sports</span>
+              <p className="CategoryBanner-BannerHoverDesc">Incredible Word of all Sports in one place. Don’t wait, Start Now!</p>
+              <span className="Button Button-ButtonPrimary">let the game begin</span>
+            </span>
+          </span>
+          <img src="img/sports.jpg" alt="" className="CategoryBanner-BannerBkg" />
+          <h1 className="CategoryBanner-BannerCategoryName">Sports</h1>
         </a>
       </div>
     );
