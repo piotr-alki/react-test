0c78ee407e42dbfbbf13ffee0982087c543dc036
Piotr Baran <piotr.alki@gmail.com>
2017-03-30 12:56:46 +0200

    update

--- src/app/components/HomePage/SliderBanner.jsx
+++ src/app/components/HomePage/SliderBanner.jsx
@@ -9,11 +9,31 @@ var SliderBanner = React.createClass({
       <div className="SliderBanner">
         <div className="SliderBanner-Banner">
           <div className="SliderBanner-BannerContent">
-              <h1 className="SliderBanner-BannerTitle">bonus €100 Deposit or 1 BTC</h1>
+              <h1 className="SliderBanner-BannerTitle">Bonus €100&nbsp;Deposit Or&nbsp;1&nbsp;BTC</h1>
               <p className="SliderBanner-BannerDesc">Here's to fun! On your deposit at Bitwin, a 50% bonus up to €100 or 1 BTC!</p>
-              <a href="#" className="Button Button-ButtonSecondary">let the game begin</a>
+              <a href="#" className="Button Button-ButtonSecondary">Check</a>
           </div>
           <img src="img/slider-banner.jpg" alt="" className="SliderBanner-BannerBkg" />
+        </div>
+
+        <div className="SliderBanner-Navigation">
+          <a href="#" className="ButtonRound Button-ButtonDefault Button-ButtonMedium">
+            <i className="Icon Icon-IconArrowLeft"></i>
+          </a>
+          <a href="#" className="ButtonRound Button-ButtonSecondary SliderBanner-NavigationNext">
+            <i className="Icon Icon-IconArrowRight"></i>
+          </a>
+        </div>
+        <div className="SliderBanner-Paginator">
+          <div className="SliderBanner-PaginatorDottedBox">
+            <a href="#" className="SliderBanner-PaginatorDotted SliderBanner-PaginatorDottedActive"></a>
+            <a href="#" className="SliderBanner-PaginatorDotted SliderBanner-PaginatorDotted"></a>
+            <a href="#" className="SliderBanner-PaginatorDotted SliderBanner-PaginatorDotted"></a>
+          </div>
+          <div className="SliderBanner-PaginatorCount">
+            <span className="SliderBanner-PaginatorCount-Active">1</span>
+            <sup>/5</sup>
+          </div>
         </div>
       </div>
     );
